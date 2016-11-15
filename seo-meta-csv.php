ba75b17628a1ab80261a471b9bffaacd04e6b46f
Danemm Fabrigar <danemmf@surefiresocial.com>
2016-11-15 11:51:06 -0800

    Initial files and folders added

---
+++ seo-meta-csv.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Plugin Name: SEO Meta CSV
+ * Version: 1.0
+ * Plugin URI: https://github.com/danemmfabz16/seo-meta-csv.git
+ * Description: CSV uploader that integrates to Yoast SEO plugin.
+ * Author: FabzWebz
+ * Author URI: #
+ * Text Domain: seo-meta-csv
+ * License: GPL v3
+ */
+
